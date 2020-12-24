8a38963acfb2b85afd487b516e04f836a2ef7523
Sam Littlefair <sam.littlefair@prismic.io>
2020-12-24 10:57:49 +0100

    Change nav key

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,10 +2,10 @@ import { default as NextLink } from 'next/link'
 import { RichText } from 'prismic-reactjs'
 import { linkResolver, hrefResolver } from 'prismic-configuration'
 
-const MenuLinks = ({ links = [] }) => (
-  links.map((menuLink) => {
+const MenuLinks = ({ links = [] }) =>
+  links.map((menuLink, index) => {
     return (
-      <li key={menuLink.link.id}>
+      <li key={index}>
         <NextLink
           as={linkResolver(menuLink.link)}
           href={hrefResolver(menuLink.link)}
@@ -15,12 +15,9 @@ const MenuLinks = ({ links = [] }) => (
           <a>{RichText.asText(menuLink.label)}</a>
         </NextLink>
       </li>
-    );
+    )
   })
-);
 
-const Navigation = ({ menu }) => (
-  <MenuLinks links={menu.data.menu_links} />
-);
+const Navigation = ({ menu }) => <MenuLinks links={menu.data.menu_links} />
 
-export default Navigation;
+export default Navigation
